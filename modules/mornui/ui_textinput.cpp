5eb75069849542a67b1a0ad0f130ecb7cd7238b3
c <c@112.com>
2019-02-20 20:01:26 +0800

    。。。

--- modules/mornui/ui_textinput.cpp
+++ modules/mornui/ui_textinput.cpp
@@ -1,5 +1,7 @@
 #include "ui_textinput.h"
 #include "core/os/os.h"
+#include "core/os/keyboard.h"
+#include "core/message_queue.h"
 
 UI_TextInput::UI_TextInput() {
 	set_focus_mode(FOCUS_ALL);
@@ -45,20 +47,180 @@ void UI_TextInput::InitAttribute(Ref<XMLNode> node,ScriptInstance* self) {
 }
 void UI_TextInput::_gui_input(Ref<InputEvent> p_event) {
 	Ref<InputEventMouseButton> b = p_event;
-	if (b.is_valid()) {
+	if (b.is_valid() && b->get_button_index() == BUTTON_LEFT) {
+		_reset_caret_blink_timer();
 		if (b->is_pressed()) {
-			if (clipY_ > 1) {
-				index_ = 2;
+			shift_selection_check_pre(b->get_shift());
+			set_cursor_at_pixel_pos(b->get_position().x);
+			if (b->get_shift()) {
+
+				selection_fill_at_cursor();
+				selection.creating = true;
+
 			}
+			else {
+
+				if (b->is_doubleclick()) {
+
+					selection.enabled = true;
+					selection.begin = 0;
+					selection.end = text.length();
+					selection.doubleclick = true;
+				}
+
+				selection.drag_attempt = false;
+
+				if ((cursor_pos < selection.begin) || (cursor_pos > selection.end) || !selection.enabled) {
+
+					deselect();
+					selection.cursor_start = cursor_pos;
+					selection.creating = true;
+				}
+				else if (selection.enabled) {
+
+					selection.drag_attempt = true;
+				}
+			}
+
 		}
 		else {
-			index_ = 1;
+			if ((!selection.creating) && (!selection.doubleclick)) {
+				deselect();
+			}
+			selection.creating = false;
+			selection.doubleclick = false;
+
+			if (OS::get_singleton()->has_virtual_keyboard())
+				OS::get_singleton()->show_virtual_keyboard(text, get_global_rect());
 		}
+
 		update();
+	}
+
+	Ref<InputEventMouseMotion> m = p_event;
+	if (m.is_valid()) {
+
+		if (m->get_button_mask() & BUTTON_LEFT) {
+
+			if (selection.creating) {
+				set_cursor_at_pixel_pos(m->get_position().x);
+				selection_fill_at_cursor();
+			}
+		}
+	}
+
+	Ref<InputEventKey> k = p_event;
+	if (k.is_valid()) {
+		if (!k->is_pressed())
+			return;
+
+		unsigned int code = k->get_scancode();
+
+		if (k->get_command()) {
+
+			bool handled = true;
+
+			switch (code) {
+
+			case (KEY_X): { // CUT
+
+				if (editable) {
+					cut_text();
+				}
+
+			} break;
+
+			case (KEY_C): { // COPY
+
+				copy_text();
+
+			} break;
+
+			case (KEY_V): { // PASTE
+
+				if (editable) {
+
+					paste_text();
+				}
+
+			} break;
+
+			case (KEY_Z): { // undo / redo
+				if (editable) {
+					if (k->get_shift()) {
+						redo();
+					}
+					else {
+						undo();
+					}
+				}
+			} break;
+
+			case (KEY_U): { // Delete from start to cursor
+
+				if (editable) {
+
+					deselect();
+					text = text.substr(cursor_pos, text.length() - cursor_pos);
+
+					Ref<Font> font = get_font("font");
+
+					cached_width = 0;
+					if (font != NULL) {
+						for (int i = 0; i < text.length(); i++)
+							cached_width += font->get_char_size(text[i]).width;
+					}
+
+					set_cursor_position(0);
+					_text_changed();
+				}
+
+			} break;
+
+			case (KEY_Y): { // PASTE (Yank for unix users)
+
+				if (editable) {
+
+					paste_text();
+				}
+
+			} break;
+			case (KEY_K): { // Delete from cursor_pos to end
+
+				if (editable) {
+
+					deselect();
+					text = text.substr(0, cursor_pos);
+					_text_changed();
+				}
+
+			} break;
+			case (KEY_A): { //Select All
+				select();
+			} break;
+#ifdef APPLE_STYLE_KEYS
+			case (KEY_LEFT): { // Go to start of text - like HOME key
+				set_cursor_position(0);
+			} break;
+			case (KEY_RIGHT): { // Go to end of text - like END key
+				set_cursor_position(text.length());
+			} break;
+#endif
+			default: {
+				handled = false;
+			}
+			}
+
+			if (handled) {
+				accept_event();
+				return;
+			}
+		}
+
 	}
 }
 void UI_TextInput::_notification(int p_what) {
-	if (!button_draw_)return;
+	if (!textinput_draw_)return;
 	if (p_what == NOTIFICATION_MOUSE_ENTER) {
 		if (clipY_ > 1) {
 			index_ = 1;
@@ -92,6 +254,219 @@ void UI_TextInput::_notification(int p_what) {
 		}
 	}
 }
+void UI_TextInput::selection_fill_at_cursor() {
+
+	selection.begin = cursor_pos;
+	selection.end = selection.cursor_start;
+
+	if (selection.end < selection.begin) {
+		int aux = selection.end;
+		selection.end = selection.begin;
+		selection.begin = aux;
+	}
+
+	selection.enabled = (selection.begin != selection.end);
+}
+
+void UI_TextInput::shift_selection_check_pre(bool p_shift) {
+
+	if (!selection.enabled && p_shift) {
+		selection.cursor_start = cursor_pos;
+	}
+	if (!p_shift)
+		deselect();
+}
+
+void UI_TextInput::select(int p_from, int p_to) {
+
+	if (p_from == 0 && p_to == 0) {
+		deselect();
+		return;
+	}
+
+	int len = text.length();
+	if (p_from < 0)
+		p_from = 0;
+	if (p_from > len)
+		p_from = len;
+	if (p_to < 0 || p_to > len)
+		p_to = len;
+
+	if (p_from >= p_to)
+		return;
+
+	selection.enabled = true;
+	selection.begin = p_from;
+	selection.end = p_to;
+	selection.creating = false;
+	selection.doubleclick = false;
+	update();
+}
+
+void UI_TextInput::select_all() {
+
+	if (!text.length())
+		return;
+
+	selection.begin = 0;
+	selection.end = text.length();
+	selection.enabled = true;
+	update();
+}
+
+
+void UI_TextInput::selection_delete() {
+
+	if (selection.enabled)
+		delete_text(selection.begin, selection.end);
+
+	deselect();
+}
+
+void UI_TextInput::deselect() {
+
+	selection.begin = 0;
+	selection.end = 0;
+	selection.cursor_start = 0;
+	selection.enabled = false;
+	selection.creating = false;
+	selection.doubleclick = false;
+	update();
+}
+
+void UI_TextInput::undo() {
+	if (undo_stack_pos == NULL) {
+		if (undo_stack.size() <= 1) {
+			return;
+		}
+		undo_stack_pos = undo_stack.back();
+	}
+	else if (undo_stack_pos == undo_stack.front()) {
+		return;
+	}
+	undo_stack_pos = undo_stack_pos->prev();
+	TextOperation op = undo_stack_pos->get();
+	text = op.text;
+	set_cursor_position(op.cursor_pos);
+	_emit_text_change();
+}
+
+void UI_TextInput::redo() {
+	if (undo_stack_pos == NULL) {
+		return;
+	}
+	if (undo_stack_pos == undo_stack.back()) {
+		return;
+	}
+	undo_stack_pos = undo_stack_pos->next();
+	TextOperation op = undo_stack_pos->get();
+	text = op.text;
+	set_cursor_position(op.cursor_pos);
+	_emit_text_change();
+}
+
+void UI_TextInput::cut_text() {
+
+	if (selection.enabled && !pass) {
+		OS::get_singleton()->set_clipboard(text.substr(selection.begin, selection.end - selection.begin));
+		selection_delete();
+	}
+}
+
+void UI_TextInput::copy_text() {
+
+	if (selection.enabled && !pass) {
+		OS::get_singleton()->set_clipboard(text.substr(selection.begin, selection.end - selection.begin));
+	}
+}
+
+void UI_TextInput::paste_text() {
+
+	String paste_buffer = OS::get_singleton()->get_clipboard();
+
+	if (paste_buffer != "") {
+
+		if (selection.enabled) selection_delete();
+		append_at_cursor(paste_buffer);
+
+		if (!text_changed_dirty) {
+			if (is_inside_tree()) {
+				MessageQueue::get_singleton()->push_call(this, "_text_changed");
+			}
+			text_changed_dirty = true;
+		}
+	}
+}
+
+void UI_TextInput::delete_text(int p_from_column, int p_to_column) {
+
+	if (text.size() > 0) {
+		Ref<Font> font = get_font("font");
+		if (font != NULL) {
+			for (int i = p_from_column; i < p_to_column; i++)
+				cached_width -= font->get_char_size(text[i]).width;
+		}
+	}
+	else {
+		cached_width = 0;
+	}
+
+	text.erase(p_from_column, p_to_column - p_from_column);
+	cursor_pos -= CLAMP(cursor_pos - p_from_column, 0, p_to_column - p_from_column);
+
+	if (cursor_pos >= text.length()) {
+
+		cursor_pos = text.length();
+	}
+	if (window_pos > cursor_pos) {
+
+		window_pos = cursor_pos;
+	}
+
+	if (!text_changed_dirty) {
+		if (is_inside_tree()) {
+			MessageQueue::get_singleton()->push_call(this, "_text_changed");
+		}
+		text_changed_dirty = true;
+	}
+}
+
+void UI_TextInput::clear() {
+
+	clear_internal();
+	_text_changed();
+}
+void UI_TextInput::clear_internal() {
+
+	_clear_undo_stack();
+	cached_width = 0;
+	cursor_pos = 0;
+	window_pos = 0;
+	undo_text = "";
+	text = "";
+	update();
+}
+
+void UI_TextInput::_clear_undo_stack() {
+	undo_stack.clear();
+	undo_stack_pos = NULL;
+	_create_undo_state();
+}
+
+void UI_TextInput::_create_undo_state() {
+	TextOperation op;
+	op.text = text;
+	op.cursor_pos = cursor_pos;
+	undo_stack.push_back(op);
+}
+
+void UI_TextInput::_reset_caret_blink_timer() {
+	if (caret_blink_enabled) {
+		draw_caret = true;
+		update();
+	}
+}
+
 void UI_TextInput::set_cursor_at_pixel_pos(int p_x) {
 
 	Ref<Font> font = get_font("font");
@@ -137,6 +512,7 @@ void UI_TextInput::set_cursor_at_pixel_pos(int p_x) {
 
 	set_cursor_position(ofs);
 }
+
 void UI_TextInput::set_cursor_position(int p_pos) {
 
 	if (p_pos > (int)text.length())
@@ -195,6 +571,27 @@ void UI_TextInput::set_cursor_position(int p_pos) {
 	}
 	update();
 }
+
+void UI_TextInput::append_at_cursor(String p_text) {
+
+	if ((max_length <= 0) || (text.length() + p_text.length() <= max_length)) {
+
+		Ref<Font> font = get_font("font");
+		if (font != NULL) {
+			for (int i = 0; i < p_text.length(); i++)
+				cached_width += font->get_char_size(p_text[i]).width;
+		}
+		else {
+			cached_width = 0;
+		}
+
+		String pre = text.substr(0, cursor_pos);
+		String post = text.substr(cursor_pos, text.length() - cursor_pos);
+		text = pre + p_text + post;
+		set_cursor_position(cursor_pos + p_text.length());
+	}
+}
+
 void UI_TextInput::set_window_pos(int p_pos) {
 
 	window_pos = p_pos;
--- modules/mornui/ui_textinput.h
+++ modules/mornui/ui_textinput.h
@@ -23,6 +23,18 @@ public:
 	void set_cursor_at_pixel_pos(int p_x);
 	void set_cursor_position(int p_pos);
 	void set_window_pos(int p_pos);
+	void deselect();
+	void copy_text();
+	void cut_text();
+	void paste_text();
+	void undo();
+	void redo();
+	void select(int p_from = 0, int p_to = -1);
+	void select_all();
+	void selection_delete();
+	void delete_text(int p_from_column, int p_to_column);
+	void append_at_cursor(String p_text);
+	void clear();
 protected:
 	static void _bind_methods();
 	void _notification(int p_what);
@@ -30,16 +42,43 @@ protected:
 	//virtual void _unhandled_input(Ref<InputEvent> p_event);
 	//static void _bind_methods();
 	String xl_text{"a\nb\nc\nd"};
-	bool button_draw_{ true };
+	bool textinput_draw_{ true };
 	bool select_{ false };
 private:
+	struct Selection {
+
+		int begin;
+		int end;
+		int cursor_start;
+		bool enabled;
+		bool creating;
+		bool doubleclick;
+		bool drag_attempt;
+	} selection;
+
 	Align align{ ALIGN_LEFT };
-	bool editable;
-	bool pass;
+	bool editable{ false };
+	bool pass{ false };
 	bool text_changed_dirty;
 	int window_pos{ 0 };
 	int cached_width{ 0 };
 	int cursor_pos{ 0 };
+	bool caret_blink_enabled{ false };
+	bool draw_caret{ false };
+	struct TextOperation {
+		int cursor_pos;
+		String text;
+	};
+	List<TextOperation> undo_stack;
+	List<TextOperation>::Element *undo_stack_pos;
 	String text;
+	String undo_text;
+
+	void _clear_undo_stack();
+	void _create_undo_state();
+	void _reset_caret_blink_timer();
+	void shift_selection_check_pre(bool);
+	void selection_fill_at_cursor();
+	void clear_internal();
 };
 #endif
